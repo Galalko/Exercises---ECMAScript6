b8aa392f46ea776c7644f3ededcb1c5153bab560
Galalko <85119375+Galalko@users.noreply.github.com>
2021-07-07 00:54:46 +0200

    Add files via upload

---
+++ Exercises .js
@@ -0,0 +1,96 @@
+// 1 
+/*
+let user = {
+    name: "John",
+    years: 30
+  }
+  console.log(user)
+*/
+
+  
+
+
+
+// Exercise 2    Give the right name
+ // Create the variable with the name of our planet. How would you name such a variable?                          
+ // Create the variable to store the name of the current visitor. How would you name that variable?                
+
+    var ouPlanet ="Earth"
+    var currentVisitor = {}
+
+
+
+ // 3
+ let phrase = "Hello"
+
+if (true) {
+  let user = "John";
+  function sayHi() {
+    alert(`${phrase}, ${user}`)
+  }
+}
+
+sayHi()
+// the result will be (Hello John)
+// why ? because the if statment is true and the function will work .
+
+
+
+
+// Exercise 4       Write the code, one line for each action:
+/*
+Create an empty object user.                                               
+Add the property name with the value John.                                 
+Add the property surname with the value Smith.                           
+Change the value of the name to Pete.                                   
+Remove the property name from the object.
+
+1/ let user = {}
+2/  let user = {
+     username = "John";
+}
+3/     let user = {
+        name: 'John',
+        surname: 'Smith'
+}
+
+4/  let user = {
+        name: 'John',
+        surname: 'Smith'
+}
+let name = 'pete'
+user.name =name
+*/
+
+
+
+
+
+// Exercise 5
+/*const user = {
+    name: "John"
+  }
+  
+  // does it work?
+  user.name = "Pete"*/
+// Ito change an object declared with const, how do you think and why?             >>>> yes its possible  , i thinks because  you changed the variable inside the constat not the main constat 
+
+
+
+
+
+
+
+
+//8 
+/*      var  message='';
+        var login = 'Director';
+
+        login == 'Employee' ? console.log('Hello')
+        : login == 'Director' ?console.log('Greetings')
+        : login == ''? console.log('No Login')
+        : console.log('')
+
+
+
+*/
